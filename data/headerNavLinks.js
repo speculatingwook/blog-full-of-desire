dfe18a5e6d0030084ff12a9291e5f0e04b428793
Brian <bwook9908@gmail.com>
2023-04-14 11:42:44 +0900

    header changed

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,8 +1,8 @@
 const headerNavLinks = [
-  { href: '/blog', title: 'Articles' },
-  { href: '/projects', title: 'Projects' },
-  { href: '/tags', title: 'Tags' },
-  { href: '/about', title: 'About' },
+  { href: '/blog', title: '<Articles/>' },
+  { href: '/projects', title: '<Projects/>' },
+  { href: '/tags', title: '<Tags/>' },
+  { href: '/about', title: '<About/>' },
 ]
 
 export default headerNavLinks
