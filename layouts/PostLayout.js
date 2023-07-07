0bb17a2aaf72a49c56336ecc91e31b307399eecc
Brian <bwook9908@gmail.com>
2023-07-07 17:52:19 +0900

    feat: osi 7 layer

--- layouts/PostLayout.js
+++ layouts/PostLayout.js
@@ -71,13 +71,23 @@ export default function PostLayout({ frontMatter, authorDetails, next, prev, chi
                         <dt className="sr-only">Name</dt>
                         <dd className="text-gray-900 dark:text-gray-100">{author.name}</dd>
                         <dt className="sr-only">Twitter</dt>
+                        {/*<dd>*/}
+                        {/*  {author.twitter && (*/}
+                        {/*    <Link*/}
+                        {/*      href={author.twitter}*/}
+                        {/*      className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"*/}
+                        {/*    >*/}
+                        {/*      {author.twitter.replace('https://twitter.com/', '@')}*/}
+                        {/*    </Link>*/}
+                        {/*  )}*/}
+                        {/*</dd>*/}
                         <dd>
-                          {author.twitter && (
+                          {author.github && (
                             <Link
-                              href={author.twitter}
+                              href={author.github}
                               className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
                             >
-                              {author.twitter.replace('https://twitter.com/', '@')}
+                              {author.github.replace('https://github.com/', '@')}
                             </Link>
                           )}
                         </dd>
